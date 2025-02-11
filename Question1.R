5b69e4db74a945b206b31193e4cac8777e8d5a2d
Zoe Azra <zoe@blei.nu>
2025-02-11 17:38:27 +0100

    Adding more to Q1B

--- Question1.R
+++ Question1.R
@@ -6,7 +6,7 @@ library(ggplot2)
 set.seed(0)
 data = read.delim("cholesterol.txt", sep=' ')
 
-# Question 1A:
+################## QUESTION 1A #####################
 
 # A scatter plot for correlation between Before and After8weeks
 plot(data$Before, data$After8weeks, 
@@ -38,10 +38,33 @@ hist(data$After8weeks, probability = TRUE,
      ylab = 'Probability')
 lines(density(data$Before), col = "red", lwd = 2)
 
-# Question 1B:
+
+##################### QUESTION 1B ########################
+
+# H0: The margarine diet has no effect, i.e. the mean cholesterol levels Before and After 8 weeks are the same
+# H1: The margarine diet reduces cholesterol levels --> mean_before > mean_after
+# Paired t-test
+# Assumption: the differences between Before and After should be normally distributed
+# Outcome: if p < 0.05, reject H0
+t.test(data$Before, data$After8weeks, paired = TRUE, alternative = "greater")
+
+# Visualizing distribution of the differences
+difference = data$Before - data$After8weeks
+
+hist(difference, probability = TRUE,
+     main = 'Data distribution of differences between Before and After8weeks',
+     col = 'lightblue',
+     xlab = 'Difference (Before - After8weeks)',
+     ylab = 'Proportion')
+lines(density(difference), col = 'red', lwd = 2)
+
+qqnorm(difference,
+       main = 'QQ-plot for differences',
+       xlab = 'Theoretical Quantiles',
+       ylab = 'Sample Quantiles')
+qqline(difference, col = "red")
+
+# Testing normality of the data
 # If p > 0.05 --> fail to reject normality (data is normal)
-shapiro.test(data$Before)
-shapiro.test(data$After8weeks)
-
-
+shapiro.test(difference)
 
