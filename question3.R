08c604275e17ffb7854673326efb7cb9080c1917
fredmloth <frederiekeloth@gmail.com>
2025-02-15 21:12:04 +0100

    create data frame instead of grid for later changes and additions

--- question3.R
+++ question3.R
@@ -2,9 +2,9 @@
 # install.packages('MASS')
 
 # Set block and plot dimensions
-blocks <- 1:6
-plots <- 4
-additives <- 3
+n_blocks <- 1:6
+n_plots <- 4
+additives <- c("N", "P", "K")
 
 # Create all possible combinations
 possible_plots <- expand.grid(N = c(0, 1), P = c(0, 1), K = c(0, 1))
@@ -13,8 +13,8 @@ possible_plots <- expand.grid(N = c(0, 1), P = c(0, 1), K = c(0, 1))
 # Randomly assign combinations
 random_additives <- list()
 
-for (block in blocks) {
-  random_additives[[block]] <- sample(1:nrow(possible_plots), plots, replace = TRUE)
+for (block in n_blocks) {
+  random_additives[[block]] <- sample(1:nrow(possible_plots), n_plots, replace = TRUE)
 }
 
 random_additives
@@ -22,9 +22,10 @@ random_additives
 # Less computational way to assign randomly but adhere to max 2
 
 # Create empty 4x3 grid
-for (block in blocks) {
-  block_grid <- matrix(0, nrow = plots, ncol = additives)
+for (block in n_blocks) {
+  blocks <- data.frame(matrix(0, nrow = n_plots, ncol = length(additives)))
+  colnames(blocks) <- additives
 }
 
-block_grid
+blocks
 
