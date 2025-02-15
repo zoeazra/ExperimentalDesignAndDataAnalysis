4c1f0bb171e980061f067f63b8974dd06d90336f
fredmloth <frederiekeloth@gmail.com>
2025-02-15 21:08:24 +0100

    create matrix grid to randomly distribute 2 of each additive

--- question3.R
+++ question3.R
@@ -4,6 +4,7 @@
 # Set block and plot dimensions
 blocks <- 1:6
 plots <- 4
+additives <- 3
 
 # Create all possible combinations
 possible_plots <- expand.grid(N = c(0, 1), P = c(0, 1), K = c(0, 1))
@@ -17,4 +18,13 @@ for (block in blocks) {
 }
 
 random_additives
+
+# Less computational way to assign randomly but adhere to max 2
+
+# Create empty 4x3 grid
+for (block in blocks) {
+  block_grid <- matrix(0, nrow = plots, ncol = additives)
+}
+
+block_grid
 
