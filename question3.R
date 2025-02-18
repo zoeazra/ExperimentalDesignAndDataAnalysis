1564cb047148246f0252c4d36c15bde9c453d712
fredmloth <frederiekeloth@gmail.com>
2025-02-18 22:28:47 +0100

    performed two-way anova

--- question3.R
+++ question3.R
@@ -11,6 +11,8 @@ library(ggplot2)
 data(npk)
 
 # START OF 3.a
+# The yield is not taking into account due to possible unkown variance/ 
+# dependence on factors.
 
 # Set block and plot dimensions
 n_blocks <- 1:6
@@ -43,6 +45,8 @@ random_distributed
 
 
 # START OF 3.b
+# The yield is only (at this moment) possible to be collected from the dataset 
+# and cannot be randomized due to unkown variance/ dependencies.
 
 # Group npk data by block and N
 grouped_data <- group_by(npk, block, N)
@@ -64,4 +68,31 @@ avg_yield_plot <- ggplot(average_yield_block, aes(x = block, y = Average_Yield, 
 avg_yield_plot
 
 # START OF 3.c
+
+# ANOVA (two-way)
+# Indpenedent factors: block and N
+# response variable: yield
+# perform test to check normality
+
+# Interpreting the table
+# Df = degrees of freedom
+# sum of squares = total var from blocks
+# mean sq = sum of squares for block/degrees of freedom
+# F value = ratio of variation from block to residual
+  # 3.395 times larger than residuals
+#Pr(>F) = p-value for if block significant effect on yield (response variable)
+  # Less than 0.05 so statistically significant (5% level)
+
+# F value N: larger effect on yield compared to block
+# P-value of N is highly significant (1% level)
+
+# residuals sum sq represents variation, unexplainable by block or N
+
+# Both N and p have statistically significant effect, but Nitrogen has a higher 
+# effect on yield.
+
+two_way_anova <- aov(yield ~ block + N, data = npk)
+summary(two_way_anova)
+
+
 
