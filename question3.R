6997009645f2610c75c7f6e6dd78ad0a15068cb7
fredmloth <frederiekeloth@gmail.com>
2025-02-19 14:33:10 +0100

    comments with observations, needs cleaning

--- question3.R
+++ question3.R
@@ -13,6 +13,7 @@ data(npk)
 # START OF 3.a
 # The yield is not taking into account due to possible unkown variance/ 
 # dependence on factors.
+# rewrite to output data similar to the npk set
 
 # Set block and plot dimensions
 n_blocks <- 1:6
@@ -69,6 +70,15 @@ avg_yield_plot
 
 # START OF 3.c
 
+datac <- npk; npk$block <- as.factor(npk$block); npk$N <- as.factor(npk$N)
+dfc <- npk[c("block", "N", "yield")]
+modelc <- lm(yield ~ block * N, data = dfc); anova(modelc)
+interaction.plot(dfc$N,dfc$block,dfc$yield)
+interaction.plot(dfc$block,dfc$N,dfc$yield)
+
+
+
+
 # ANOVA (two-way)
 # Indpenedent factors: block and N
 # response variable: yield
@@ -121,12 +131,29 @@ shapiro.test(residuals(two_way_anova))
 
 #Only main effects, without interaction terms
 # assumes no interaction between components 
+
 # Effect of each on yield
 model_1 <- aov(yield ~ N + P + K + block, data = npk)
 summary(model_1)
+#N, K and block have a significant effect on yield
+# P has no significant effect so omitted
 
-model_2 <- aov(yield ~ P * K + K * block, data = npk)
+# Interactions between N, K and block
+model_2 <- aov(yield ~ N * K + N * block + K * block, data = npk)
 summary(model_2)
 
 # Seems only N really has a significatnt effect, combining models to showcase 
 # that is prefered?
+# Not everything due to overfitting
+
+# Seems there is no interaction?
+# Check
+
+
+
+# Critical F-value
+qf(1 - 0.05, df1 = 1, df2 = 10)
+qf(1 - 0.05, df1 = 5, df2 = 10)
+
+# START OF 3.e
+
