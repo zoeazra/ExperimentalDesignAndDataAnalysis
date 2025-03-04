b769312023788308b31cd82ab0619f7c04613eb1
fredmloth <frederiekeloth@gmail.com>
2025-03-04 14:35:19 +0100

    started poisson, removing insignificant (based on p-value) one-by-one

--- a2_fred_test.R
+++ a2_fred_test.R
@@ -6,3 +6,16 @@ data <- read.table("coups.txt", header = TRUE, row.names = 1)
 data
 
 str(data)
+
+# To access full dataset use .
+#miltcoup is response var
+# Use glm since counting, lm not correct
+poisson_reg <- glm (miltcoup ~ ., data = data, family = poisson)
+summary(poisson_reg)
+
+# Remove insignificant pieces one by one (summary)
+
+poisson_reg1 <- glm(miltcoup ~ oligarchy + pollib + parties + pctvote + popn + size + numregim, 
+                    data = data,
+                    family = poisson)
+summary(poisson_reg1)        
