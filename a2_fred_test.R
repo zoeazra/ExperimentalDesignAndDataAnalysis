93adba00c3a24583e3c68d7668e2bb0678b11337
fredmloth <frederiekeloth@gmail.com>
2025-03-04 14:45:22 +0100

    removing when p>0.05

--- a2_fred_test.R
+++ a2_fred_test.R
@@ -14,8 +14,28 @@ poisson_reg <- glm (miltcoup ~ ., data = data, family = poisson)
 summary(poisson_reg)
 
 # Remove insignificant pieces one by one (summary)
-
 poisson_reg1 <- glm(miltcoup ~ oligarchy + pollib + parties + pctvote + popn + size + numregim, 
                     data = data,
                     family = poisson)
 summary(poisson_reg1)        
+
+poisson_reg2 <- glm(miltcoup ~ oligarchy + pollib + parties + pctvote + popn + numregim, 
+                    data = data,
+                    family = poisson)
+summary(poisson_reg2)  
+
+poisson_reg3 <- glm(miltcoup ~ oligarchy + pollib + parties + pctvote + popn, 
+                    data = data,
+                    family = poisson)
+summary(poisson_reg3)  
+
+poisson_reg4 <- glm(miltcoup ~ oligarchy + pollib + parties + pctvote, 
+                    data = data,
+                    family = poisson)
+summary(poisson_reg4)
+
+
+poisson_reg5 <- glm(miltcoup ~ oligarchy + pollib + parties, 
+                    data = data,
+                    family = poisson)
+summary(poisson_reg5)
